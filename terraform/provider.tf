9a4482f71a536fb476b522d879cd8e230e46974b
Priya Mirdha <priyamirdha05.jsr@gmail.com>
2026-09-24 12:15:18 +0530

    modified provider-subnet

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
 }
+
+backend "s3" {
+    bucket = "java-devops-terraform-state-354147111180"
+    key    = "java-devops/terraform.tfstate"
+    region = "ap-south-1"
+  }
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "eks_subnet_2" {
    vpc_id = "vpc-0603e2c4c78d4d7f2"
-   cidr_block = "172.31.1.0/24"
+   cidr_block = "172.32.1.0/24"
    availability_zone = "ap-south-1a"
    tags ={
     Name = "eks-subnet-devops"
