d67b92ef947eb8583eeca68b5c306a9b62e9acd4
Priya Mirdha <priyamirdha05.jsr@gmail.com>
2026-09-24 12:29:38 +0530

    provider-backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
     region = "ap-south-1"
 }
-
-backend "s3" {
-    bucket = "java-devops-terraform-state-354147111180"
-    key    = "java-devops/terraform.tfstate"
-    region = "ap-south-1"
-  }
